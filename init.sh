983c3b4f06ee5d9980352e5cfeefddd9097ddec6
Federico Valeri <fedevaleri@gmail.com>
2024-02-21 14:56:47 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-INIT_NAMESPACE="test"
-INIT_KAFKA_VERSION="3.5.0"
-INIT_STRIMZI_VERSION="0.36.0"
+NAMESPACE="test"
+KAFKA_VERSION="3.5.0"
+STRIMZI_VERSION="0.36.0"
 
 if [[ "${BASH_SOURCE[0]}" -ef "$0" ]]; then
   echo "Source this script, not execute it"; exit 1
@@ -15,13 +15,12 @@ for x in kubectl yq; do
 done
 
 kubectl-kafka() { kubectl run kubectl-kafka-"$(date +%s)" -itq --rm --restart="Never" \
-  --image="quay.io/strimzi/kafka:latest-kafka-$INIT_KAFKA_VERSION" -- sh -c "$*; exit 0"; }
+  --image="quay.io/strimzi/kafka:latest-kafka-$KAFKA_VERSION" -- sh -c "$*; exit 0"; }
 
-kubectl delete ns "$INIT_NAMESPACE" target --wait &>/dev/null
-kubectl wait --for=delete ns/"$INIT_NAMESPACE" --timeout=120s &>/dev/null
-kubectl -n "$INIT_OPERATOR_NS" delete pv -l "app=retain-patch" &>/dev/null
-kubectl create ns "$INIT_NAMESPACE"
-kubectl config set-context --current --namespace="$INIT_NAMESPACE" &>/dev/null
-curl -sL "https://github.com/strimzi/strimzi-kafka-operator/releases/download/$INIT_STRIMZI_VERSION/strimzi-cluster-operator-$INIT_STRIMZI_VERSION.yaml" \
-  | sed -E "s/namespace: .*/namespace: $INIT_NAMESPACE/g" | kubectl create -f - --dry-run=client -o yaml | kubectl replace --force -f - &>/dev/null
+kubectl delete ns "$NAMESPACE" target --wait &>/dev/null
+kubectl wait --for=delete ns/"$NAMESPACE" --timeout=120s &>/dev/null
+kubectl create ns "$NAMESPACE"
+kubectl config set-context --current --namespace="$NAMESPACE" &>/dev/null
+curl -sL "https://github.com/strimzi/strimzi-kafka-operator/releases/download/$STRIMZI_VERSION/strimzi-cluster-operator-$STRIMZI_VERSION.yaml" \
+  | sed -E "s/namespace: .*/namespace: $NAMESPACE/g" | kubectl create -f - --dry-run=client -o yaml | kubectl replace --force -f - &>/dev/null
 echo "Done"
